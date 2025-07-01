17c7eaf995695230e17b6b3c93745efdaf4d4ea1
sentinal <inkdupe1@gmail.com>
2025-07-01 13:42:01 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const scripts = [
+    {
+        title: "JAB Hub",
+        desc: "a hub full with FE scripts for just a baseplate",
+        gameTitle: "Just a baseplate",
+        icon: "https://tr.rbxcdn.com/180DAY-e63d816e6a73615478eab2b32968b4a8/256/256/Image/Webp/noFilter",
+        thumbnail: "https://tr.rbxcdn.com/180DAY-233a115e47f0bfd54757e1c03fbea1c1/768/432/Image/Webp/noFilter",
+        code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/inkdupe/hat-scripts/refs/heads/main/JABhub"))()'
+    },
+    {
+        title: "700 Players in the same server",
+        desc: "auto roll, auto sell, inf cash",
+        gameTitle: "700 Players in the same server",
+        icon: "https://tr.rbxcdn.com/180DAY-f42675d7a99b59acad173a8bf1bc8e9b/512/512/Image/Webp/noFilter",
+        thumbnail: "https://tr.rbxcdn.com/180DAY-9ede21218a883c5732f200335733e559/768/432/Image/Webp/noFilter",
+        code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/inkdupe/scripts/refs/heads/main/700Players.lua"))()'
+    }
+];
