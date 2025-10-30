6ab3838eece6acd41c7823e38b2d5a203e3df3c0
veser <inkdupe1@gmail.com>
2025-10-30 14:27:27 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,5 +14,13 @@ const scripts = [
         icon: "https://tr.rbxcdn.com/180DAY-f42675d7a99b59acad173a8bf1bc8e9b/512/512/Image/Webp/noFilter",
         thumbnail: "https://tr.rbxcdn.com/180DAY-9ede21218a883c5732f200335733e559/768/432/Image/Webp/noFilter",
         code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/inkdupe/scripts/refs/heads/main/700Players.lua"))()'
+    },
+    {
+        title: "VMC Hub",
+        desc: "very pro hub for mad city chapter 1",
+        gameTitle: "Mad City: Chapter 1 [MALL HEIST]",
+        icon: "https://tr.rbxcdn.com/180DAY-72ab13bbc775514983b4b750007e747f/256/256/Image/Webp/noFilter",
+        thumbnail: "https://tr.rbxcdn.com/180DAY-dc9501c60d63f8121c61388eb9e9f570/768/432/Image/Webp/noFilter",
+        code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/inkdupe/scripts/refs/heads/main/VMChub.lua"))() '
     }
 ];
